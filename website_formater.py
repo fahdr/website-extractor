137bcda9ded8b46908b260530890ff70b5826f3c
fahdr <fahdfreak.gm@gmail.com>
2021-05-26 10:08:20 +0530

    added code to format data

--- website_formater.py
+++ website_formater.py
@@ -7,13 +7,15 @@ def kpophearts(product):
     sku_type=['color','team','singer','none','quantity']
     #todo: clean up all dict items
 
-
+    formatted_product_list=[]
     #splitting sku into each item
-    team='set'
-    title='title'
-    pic='pic'
-    price='price'
-    category='category'
+    for item in product[items]:
 
-    formatted_product=dict(title=title,team=team,pic=pic,price=price,category=category)
-    return formatted_product
+        team=item['item_title']
+        title=item['title']
+        pic=item['img_url']
+        price=item['item_price']
+        category=item['category']
+        formatted_product_list.append(title=title,team=team,pic=pic,price=price,category=category)
+        
+    return formatted_product_list
