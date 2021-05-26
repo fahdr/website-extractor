d7a072eff74913993d79a4d8e5ea958bbf771937
fahdr <fahdfreak.gm@gmail.com>
2021-05-26 14:47:37 +0530

    added file structure for CRUD operations

---
+++ database.py
@@ -0,0 +1,15 @@
+from mongoengine import connect
+
+client=connect('testdb')
+
+def addAliExProduct():
+    print()
+
+def deleteAliExProduct():
+    print()
+
+def updateAliExProduct():
+    print()
+
+
+
