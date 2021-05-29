4d1989ae4c0d752a4e057bd2fe90d4ecd4f70b7d
fahdr <fahdfreak.gm@gmail.com>
2021-05-29 12:00:03 +0530

    added route to get products

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 from .extract_api import Extract
-from .product_api import Product
+from .product_api import Product,Products
 def initialize_routes(api):
     api.add_resource(Extract, '/extract/<website>/<product>')
     api.add_resource(Product, '/product/<website>/<productid>')
+    api.add_resource(Products, '/products/<website>/')
