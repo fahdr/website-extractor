79757321dec4d21b6324496c2c0e0d5bc86ab0d5
fahdr <fahdfreak.gm@gmail.com>
2021-05-29 11:59:21 +0530

    removed comments and white spaces

--- src/extract_api.py
+++ src/extract_api.py
@@ -10,15 +10,14 @@ import json
 class Extract(Resource):
     def get(self,product,website):
         extractedProduct=AliExProduct(**(ExtractProduct(product)))
-
-        #save draft
-        #format for website
+        extractedProduct.website=website
         extractedProduct.websiteProducts=formatter(website,extractedProduct)
         extractedProduct.save()
-        #return info
         return(json.loads(extractedProduct.to_json()))
     
     def put(self):
         #get info
         #save draft
         return()
+
+    
