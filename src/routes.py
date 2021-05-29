333b9a385550bcf85875413c5d00b2f2ab80a0df
fahdr <fahdfreak.gm@gmail.com>
2021-05-29 22:50:41 +0530

    single product

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,6 @@
 from .extract_api import Extract
 from .product_api import Product,Products
 def initialize_routes(api):
-    api.add_resource(Extract, '/<website>/extract/<ecomm>/<product>')
-    api.add_resource(Product, '/product/<website>/<productid>')
+    api.add_resource(Extract, '/<website>/extract/<ecomm>/<product>/')
+    api.add_resource(Product, '/<website>/product/<productid>/')
     api.add_resource(Products, '/<website>/<ecomm>/products/')
