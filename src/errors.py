7ced7db51b40706d164a5435ad2e78cc7a3184b3
fahdr <fahdfreak.gm@gmail.com>
2021-05-30 14:59:37 +0530

    added code to edit document

--- src/errors.py
+++ src/errors.py
@@ -9,6 +9,15 @@ class WebsiteDoesNotExist(HTTPException):
 class ProductExists(HTTPException):
     pass
 
+class UpdateProductError(HTTPException):
+    pass
+
+class SchemaValidationError(HTTPException):
+    pass
+
+class InternalServerError(HTTPException):
+    pass
+
 errors = {
     "ScraperDoesNotExist": {
         "message": "The scraper for this ecommerce site does not exist. Make a request?",
@@ -22,4 +31,16 @@ errors = {
         "message": "Product already exists. Try editing it instead",
         "status": 400
     },
+    "UpdateProductError": {
+        "message": "Either the product does not exist, or you have not logged in",
+        "status": 400
+    },
+    "InternalServerError": {
+        "message": "Something went wrong",
+        "status": 500
+    },
+     "SchemaValidationError": {
+         "message": "Request is missing required fields",
+         "status": 400
+     },
 }
