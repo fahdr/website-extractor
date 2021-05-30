9bf85346de8f4d10868955242dbbbeac3fc8eac6
fahdr <fahdfreak.gm@gmail.com>
2021-05-30 15:03:11 +0530

    added code to edit produt

--- src/product_api.py
+++ src/product_api.py
@@ -1,15 +1,33 @@
 from flask import Response, request
 from flask_restful import Resource
 from database.models import EcommProduct
+from errors import UpdateProductError,SchemaValidationError,InternalServerError
+from mongoengine.errors import InvalidQueryError,DoesNotExist
 
 class Product(Resource):
     def get(self,productid,website):
         product=EcommProduct.objects(skuID=productid,website=website).get().to_json()
         return Response(product, mimetype="application/json", status=200)
-    def put(self):
+
+    def put(self,productid,website):
         #get info
-        #save to db for website product
-        return ()
+        try:
+            product = EcommProduct.objects.get(skuID=productid, website=website)
+            #save to db for website product
+        
+            body = request.get_json()
+            product.update(**body)
+            return '', 200
+
+        except InvalidQueryError:
+            raise SchemaValidationError
+
+        except DoesNotExist:
+            raise UpdateProductError
+
+        except Exception:
+            raise InternalServerError  
+ 
 
 class Products(Resource):
     def get(self,website,ecomm):
